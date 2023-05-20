5006ff88ef75029c2e3b58c54e051e15203da49f
JosephBabel <theodorebabel@gmail.com>
2023-05-20 09:45:02 -0700

    Move source files to src folder

---
+++ src/Assignment1Checkboxes.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment1Checkboxes
+{
+
+	public static void main(String[] args)
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		// Test 1: verify first checkbox
+		verifyFirstCheckbox(driver);
+		
+		// Test 2: obtain total number of checkboxes
+		int numCheckboxes = getNumCheckboxes(driver);
+		System.out.println("Total number of checkboxes: " + numCheckboxes);
+		
+		driver.close();
+	}
+
+	// Select and deslect checkbox and assert its state
+	private static void verifyFirstCheckbox(WebDriver driver)
+	{
+		WebElement checkbox = driver.findElement(By.id("checkBoxOption1"));
+		checkbox.click();
+		Assert.assertTrue(checkbox.isSelected(), "The checkbox should be selected");
+		checkbox.click();
+		Assert.assertFalse(checkbox.isSelected(), "The checkbox should be deselected");
+		System.out.println("The first checkbox has been verified.");
+	}
+
+	// Get the total number of all checkboxes on the page
+	private static int getNumCheckboxes(WebDriver driver)
+	{
+		List<WebElement> checkboxes = driver.findElements(By.cssSelector("[type='checkbox']"));
+		return checkboxes.size();
+	}
+}
--- src/DynamicDropdown2.java
+++ src/DynamicDropdown2.java
@@ -22,7 +22,7 @@ public class DynamicDropdown2
 		
 		// Example use of parent child relationship to find origin and destination flights
 		// Click BLR origin flight
-		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click(); // 
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click();
 		Thread.sleep(1000);
 		
 		// Click MAA destination flight
