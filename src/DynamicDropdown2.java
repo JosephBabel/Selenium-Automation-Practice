48fe1d17777387c18ad28daf80ad6813da2ae1ef
JosephBabel <theodorebabel@gmail.com>
2023-05-20 10:38:17 -0700

    Select current date in dynamic dropdown menu

--- src/DynamicDropdown2.java
+++ src/DynamicDropdown2.java
@@ -1,8 +1,8 @@
 /*
  * This code uses Selenium WebDriver to automate the selection of options 
  * from dynamic dropdown menus on a webpage. It opens a Chrome browser, 
- * selects specific options for the origin and destination dropdown menus, 
- * and then closes the browser.
+ * selects specific options for the origin and destination dropdown menus,
+ * and selects the current date.
  */
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,14 +21,16 @@ public class DynamicDropdown2
 		Thread.sleep(1000);
 		
 		// Example use of parent child relationship to find origin and destination flights
-		// Click BLR origin flight
+		// Select BLR origin flight
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click();
 		Thread.sleep(1000);
 		
-		// Click MAA destination flight
+		// Select MAA destination flight
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
-	
-		driver.close();
+		Thread.sleep(1000);
+		
+		// Select current date
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-hover")).click();
 	}
 
 }
