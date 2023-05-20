9a6be764507f7b13ab71d0d2d75eda67bd303e83
JosephBabel <theodorebabel@gmail.com>
2023-05-19 17:11:34 -0700

    Create DynamicDropDown2.java

---
+++ DynamicDropDown2.java
@@ -0,0 +1,34 @@
+/*
+ * This code uses Selenium WebDriver to automate the selection of options 
+ * from dynamic dropdown menus on a webpage. It opens a Chrome browser, 
+ * selects specific options for the origin and destination dropdown menus, 
+ * and then closes the browser.
+ */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDown2
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		// Click on the origin dropdown menu
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(1000);
+		
+		// Example use of parent child relationship to find origin and destination flights
+		// Click BLR origin flight
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click(); // 
+		Thread.sleep(1000);
+		
+		// Click MAA destination flight
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+	
+		driver.close();
+	}
+
+}
