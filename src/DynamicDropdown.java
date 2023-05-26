5ed31a628a43d028acc38fa22d2d09f41657c0f0
JosephBabel <theodorebabel@gmail.com>
2023-05-25 17:10:39 -0700

    [DynamicDropdown.java] Fix formatting of header comment

--- src/DynamicDropdown.java
+++ src/DynamicDropdown.java
@@ -1,14 +1,12 @@
 /* 
- * This code demonstrates the automation of selecting values from 
- * dynamic dropdown menus using Selenium WebDriver. It utilizes the 
- * ChromeDriver to interact with the Chrome browser.
+ * This code demonstrates the automation of selecting values from dynamic dropdown menus using Selenium WebDriver. 
+ * It utilizes the ChromeDriver to interact with the Chrome browser.
  * 
  * Prerequisites:
  * 		Selenium WebDriver and ChromeDriver must be properly installed and configured.
  * 		The ChromeDriver executable file must be located at the specified path.
  * 
- * This code is for demonstration purposes and can be modified for specific use cases 
- * involving dynamic dropdowns.
+ * This code is for demonstration purposes and can be modified for specific use cases involving dynamic dropdowns.
  */
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
